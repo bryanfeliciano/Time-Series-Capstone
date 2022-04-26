edf525b3cd565b5c4f68e7ed5f95d487814e87a9
bryan <bryanfeliciano892@gmail.com>
2022-04-26 11:48:55 -0500

    Final commit

--- time_series.hs
+++ time_series.hs
@@ -104,8 +104,6 @@ mean xs = total/count
      total = (realToFrac . sum) xs
      count = (realToFrac . length) xs
 
-
-
 meanTS :: (Real a) => TS a -> Maybe Double
 meanTS (TS _ []) = Nothing
 meanTS (TS times values) = if all (== Nothing) values then Nothing else Just avg
